3ce1bd5ea40a53d464db924aa830c527f1d2f8b6
Aliaksandr Zhur <sanchesmrio1@gmail.com>
2018-10-08 18:29:57 +0300

    98% test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
-}
+    let max = 0;
+    let temp = 1;
+    for(let i =0; i < array.length; i++){
+        if (array[i] - array[i - 1] == 1){
+            temp += 1;
+        }
+        if (array[i] - array[i - 1] ==0){
+            continue;
+        }
+        if (array[i] - array[i - 1] > 1){
+            if (temp > max){
+                max = temp;
+            }
+            temp = 1;
+        }
+     }
+     return max;
+ }
+
